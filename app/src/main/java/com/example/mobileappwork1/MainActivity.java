849138c920b222537adf56259b0628f1351ab739
MrHandsomeLJN <1205197328@qq.com>
2023-04-10 01:02:40 +0800

    buttons

--- app/src/main/java/com/example/mobileappwork1/MainActivity.java
+++ app/src/main/java/com/example/mobileappwork1/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).setTitle("汇率计算器");
 
         inputTv = findViewById(R.id.InputEqu);
@@ -34,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
         buttonMap.put("7",   (Button) findViewById(R.id.Button7));
         buttonMap.put("8",   (Button) findViewById(R.id.Button8));
         buttonMap.put("9",   (Button) findViewById(R.id.Button9));
-        buttonMap.put("c"  , (Button) findViewById(R.id.ButtonC  ));
-        buttonMap.put("00" , (Button) findViewById(R.id.Button00 ));
-
         buttonMap.put("cln", (Button) findViewById(R.id.ButtonCln));
+        buttonMap.put("00" , (Button) findViewById(R.id.Button00));
+
+        buttonMap.put("res", (Button) findViewById(R.id.ButtonRes));
         buttonMap.put("equ", (Button) findViewById(R.id.ButtonEqu));
         buttonMap.put("del", (Button) findViewById(R.id.ButtonDel));
 
@@ -49,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         for(int i = 0; i < 10; i++){
             int finalI = i;
-            Objects.requireNonNull(buttonMap.get(String.valueOf(i).substring(0, 1))).
+            Objects.requireNonNull(buttonMap.get(String.valueOf(i))).
                     setOnClickListener(view -> input(String.valueOf(finalI)));
         }
         Objects.requireNonNull(buttonMap.get("dpt")).setOnClickListener(view -> input("."));
@@ -61,12 +62,7 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(buttonMap.get("del")).setOnClickListener(view -> delete());
         Objects.requireNonNull(buttonMap.get("cln")).setOnClickListener(view -> clean());
 
-        Objects.requireNonNull(buttonMap.get("00")).setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) { input("0"); input("0"); }
-                }
-        );
+        Objects.requireNonNull(buttonMap.get("00")).setOnClickListener(view -> { input("0"); input("0"); });
     }
     public void input(String ch){
         inputEqu += ch;
