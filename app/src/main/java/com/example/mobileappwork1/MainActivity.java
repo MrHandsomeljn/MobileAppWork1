aaa8bb67c68513323d537f78d7cfcda78e086299
MrHandsomeLJN <1205197328@qq.com>
2023-04-10 00:40:41 +0800

    buttons

--- app/src/main/java/com/example/mobileappwork1/MainActivity.java
+++ app/src/main/java/com/example/mobileappwork1/MainActivity.java
@@ -1,16 +1,94 @@
 package com.example.mobileappwork1;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    double currInput = 0;
+    String inputEqu = "";
+    TextView inputTv;
+    ArrayList<View.OnClickListener> listeners = new ArrayList<>();
+    Map<String, Button> buttonMap = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+
+
+
+
         Objects.requireNonNull(getSupportActionBar()).setTitle("汇率计算器");
+        Log.d(TAG, "onCreate: 1");
+
+        inputTv = findViewById(R.id.InputEqu);
+        buttonMap.put("0",   (Button) findViewById(R.id.Button0));
+        buttonMap.put("1",   (Button) findViewById(R.id.Button1));
+        buttonMap.put("2",   (Button) findViewById(R.id.Button2));
+        buttonMap.put("3",   (Button) findViewById(R.id.Button3));
+        buttonMap.put("4",   (Button) findViewById(R.id.Button4));
+        buttonMap.put("5",   (Button) findViewById(R.id.Button5));
+        buttonMap.put("6",   (Button) findViewById(R.id.Button6));
+        buttonMap.put("7",   (Button) findViewById(R.id.Button7));
+        buttonMap.put("8",   (Button) findViewById(R.id.Button8));
+        buttonMap.put("9",   (Button) findViewById(R.id.Button9));
+        buttonMap.put("c"  , (Button) findViewById(R.id.ButtonC  ));
+        buttonMap.put("00" , (Button) findViewById(R.id.Button00 ));
+        buttonMap.put("re" , (Button) findViewById(R.id.ButtonRe ));
+        buttonMap.put("eq" , (Button) findViewById(R.id.ButtonEq ));
+        buttonMap.put("del", (Button) findViewById(R.id.ButtonDel));
+        buttonMap.put("dpt", (Button) findViewById(R.id.ButtonDpt));
+        buttonMap.put("add", (Button) findViewById(R.id.ButtonAdd));
+        buttonMap.put("sub", (Button) findViewById(R.id.ButtonSub));
+        buttonMap.put("mul", (Button) findViewById(R.id.ButtonMul));
+        buttonMap.put("div", (Button) findViewById(R.id.ButtonDiv));
+        Log.d(TAG, "onCreate: 2");
+
+        for(int i = 0; i < 10; i++){
+            int finalI = i;
+            Objects.requireNonNull(buttonMap.get(String.valueOf(i).substring(0, 1))).
+                    setOnClickListener(view -> input(String.valueOf(finalI).charAt(0)));
+        }
+        Objects.requireNonNull(buttonMap.get("dpt")).setOnClickListener(view -> input('.'));
+        Objects.requireNonNull(buttonMap.get("add")).setOnClickListener(view -> input('+'));
+        Objects.requireNonNull(buttonMap.get("sub")).setOnClickListener(view -> input('-'));
+        Objects.requireNonNull(buttonMap.get("mul")).setOnClickListener(view -> input('*'));
+        Objects.requireNonNull(buttonMap.get("div")).setOnClickListener(view -> input('/'));
+
+        Log.d(TAG, "onCreate: 3");
+
+        char[] inputChars = new char[]{'+', '-', '*', '/', '.'};
+        for(int i = 0; i < 14; i++){
+            int finalI = i;
+            View.OnClickListener l = view -> input(inputChars[finalI]);
+        }
+        Log.d(TAG, "onCreate: Finish");
+    }
+    public void input(char ch){
+        inputEqu += ch;
+        inputTv.setText(inputEqu);
+    }
+    public void clean(){
+        inputEqu = "";
+        inputTv.setText(inputEqu);
+    }
+    public void delete(char ch){
+        inputEqu = inputEqu.substring(0, inputEqu.length()-1);
+        inputTv.setText(inputEqu);
+    }
+    public void equal(){
+
     }
 }
